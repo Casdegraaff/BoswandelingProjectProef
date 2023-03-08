7f040e1f855c2b872e367fb4b5e362f3d66067a4
Quinn Croese <Quinncroese@gmail.com>
2023-03-08 13:27:09 +0100

    Scene Fixed on phone

--- Assets/Scripts/memory/CardManager.cs
+++ Assets/Scripts/memory/CardManager.cs
@@ -9,7 +9,7 @@ public class CardManager : MonoBehaviour {
   public const int gridCols = 3;
 
   //De positie van de kaarten van elkaar\\
-  public const float offsetX = 2f;
+  public const float offsetX = 1.5f;
   public const float offsetY = 2.5f;
 
   //De code voor de orginele kaart\\
