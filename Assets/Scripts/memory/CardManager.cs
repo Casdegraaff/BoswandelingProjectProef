f98e375cd911f58bb29dbe3f238f24061117d7f2
Quinn Croese <Quinncroese@gmail.com>
2023-03-15 11:49:20 +0100

    New cards and Score

--- Assets/Scripts/memory/CardManager.cs
+++ Assets/Scripts/memory/CardManager.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class CardManager : MonoBehaviour {
   //De Instellingen voor het aantal kaarten\\
