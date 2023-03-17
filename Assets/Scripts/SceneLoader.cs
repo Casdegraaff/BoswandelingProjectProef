28478711cb96c26a86689a766368332ab6980ac7
Ruben Dudink <rubendudink2003@gmail.com>
2023-03-17 10:25:15 +0100

    added main menu

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadScene1()
+    {
+        SceneManager.LoadScene("NutDrop");
+    }
+
+    public void LoadScene2()
+    {
+        SceneManager.LoadScene("Memory");
+    }
+
+    public void LoadScene3()
+    {
+        SceneManager.LoadScene("Jigsaw");
+    }
+
+
+}
